a29511afb34b21158f594c80acfb21191e6a0111
Alexander Belov <callmistercat@gmail.com>
2019-09-27 15:51:01 +0300

    Updates

---
+++ precache-manifest.c0ea73b5a12ff92b8d305f716cd0b331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a92136054b092089fc8e592b0d9272a",
+    "url": "/superapp/index.html"
+  },
+  {
+    "revision": "57d95deeda1b9556c23b",
+    "url": "/superapp/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "57d95deeda1b9556c23b",
+    "url": "/superapp/static/js/2.769f6aab.chunk.js"
+  },
+  {
+    "revision": "30b1ba55da509a5c4868",
+    "url": "/superapp/static/js/main.fd266b43.chunk.js"
+  },
+  {
+    "revision": "a94dd1279a716a45ced9",
+    "url": "/superapp/static/js/runtime-main.6c458d14.js"
+  }
+]);
